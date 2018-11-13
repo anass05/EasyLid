2689746a55eeff4d59803f736288764e6fa9a574
anass05 <anassda99@gmail.com>
2018-11-13 21:00:21 +0000

    lidar pyhton

--- raspberry/autonome2.py
+++ raspberry/autonome2.py
@@ -63,9 +63,11 @@ class MySend(Thread):
     detectObstacleAD = False
     detectObstacleAG = False
     detectObstacleAC = False
+    detectObstacleACproche = False
     differentiel = False
-    distanceDetectObstacleAD = 50
-    distanceDetectObstacleAG = 50
+    distanceDetectObstacleAD = 10
+    distanceDetectObstacleAG = 10
+    distanceDetectObstacleACproche = 10
     distanceDetectObstacleAC = 200
     i = 0
 	
@@ -75,7 +77,7 @@ class MySend(Thread):
         
     def run(self):
         
-        self.speed_cmd = 50
+        self.speed_cmd = 30
         self.move = 0
         self.turn = 0
         self.enable = 0
@@ -103,17 +105,19 @@ class MySend(Thread):
                 message = "UFR:" + str(distance)+ ";"
                 #print(distance)
                 if distance < MySend.distanceDetectObstacleAD and distance > 0:
-                    MySend.detectObstacleAD=True
-                else: MySend.detectObstacleAD=False
+                    MySend.detectObstacleAD = True
+                else: MySend.detectObstacleAD = False
                 # ultrason avant centre
                 distance = int.from_bytes(msg.data[4:6], byteorder='big')
                 message = "URC:" + str(distance)+ ";"
-                if MySend.i%10 ==0:
-                    print('-> '+ str(distance))
+                #if MySend.i%10 ==0:
+                    #print('-> '+ str(distance))
                 #print("------------------")
                 if distance < MySend.distanceDetectObstacleAC and distance > 0:
-                    MySend.detectObstacleAC=True
-                else: MySend.detectObstacleAC=False
+                    MySend.detectObstacleAC = True
+                elif distance<MySend.distanceDetectObstacleACproche:
+                    MySend.detectObstacleACproche = True
+                else: MySend.detectObstacleAC = False
                 MySend.detectObstacleOld = MySend.detectObstacle
                 #MySend.detectObstacle = MySend.detectObstacleAG or MySend.detectObstacleAD or MySend.detectObstacleAC
                 MySend.detectObstacle = MySend.detectObstacleAC
@@ -122,9 +126,9 @@ class MySend(Thread):
                 # position volant
                 position_volant = int.from_bytes(msg.data[0:2], byteorder='big')
                 message = "POS:" + str(position_volant)+ ";"
-                #print(message)
+                print(message)
                   
-            if MySend.detectObstacle:
+            if MySend.detectObstacle and not(MySend.detectObstacleAG) and not(MySend.detectObstacleAD) and not(MySend.detectObstacleACproche):
                 self.move = 1
                 self.enable = 1
                 differentiel = False
@@ -133,19 +137,22 @@ class MySend(Thread):
                     self.move = 1
                     self.enable = 1
                     differentiel = True
-                    if (position_volant > 1340):
+                    if (position_volant > 1350):
                         self.turn = -1
                         #print("turn right detected")
                     else:
                         self.turn = 0
+            elif MySend.detectObstacleAG or MySend.detectObstacleAD or MySend.detectObstacleACproche:
+                self.move = 0
+                self.enable = 0
             else:
                 #print("send cmd move forward")
                 self.move = 1
                 self.enable = 1
                 differentiel = False
-                if (position_volant<1600):
+                if (position_volant < 1600):
                     self.turn = 1
-                elif (position_volant>1700):
+                elif (position_volant > 1700):
                     self.turn = -1
                 else:
                     self.turn = 0
@@ -153,7 +160,7 @@ class MySend(Thread):
                     
             if self.enable:
                 if differentiel :
-                    cmd_mv_droit = (50 - self.move*self.speed_cmd) | 0x80
+                    cmd_mv_droit = (50 - self.move*self.speed_cmd - 10) | 0x80   #marche arrière
                     cmd_mv_gauche = (50 + self.move*self.speed_cmd) | 0x80
                 else:
                     cmd_mv_droit = (50 + self.move*self.speed_cmd) | 0x80
