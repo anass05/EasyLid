a8ecfd63998a2a5f51ebf70cd041b206923cf286
anass05 <anassda99@gmail.com>
2018-11-13 20:53:07 +0000

    lidarthreadtests

--- raspberry/lidar/lidar_light.py
+++ raspberry/lidar/lidar_light.py
@@ -4,37 +4,34 @@ import sys
 import signal
 from threading import Thread
 
-class Lidar(Thread)
-	def __init__(self, lidar):
-        Thread.__init__(self)
-        self.lidar=lidar
+class Lidar(Thread):
+  def __init__(self, lidar):
+    Thread.__init__(self)
+    self.lidar=lidar
         
-    def signal_handler(sig, frame):
-		print('You pressed Ctrl+C!')
-		self.lidar.stop()
-		self.lidar.stop_motor()
-		self.lidar.disconnect()
-		sys.exit(0)
+  def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    self.lidar.stop()
+    self.lidar.stop_motor()
+    self.lidar.disconnect()
+    sys.exit(0)
 		
-    def run(self):
-
-
-		info = self.lidar.get_info()
-		print(info)
-
-		health = self.lidar.get_health()
-		print(health)
-		signal.signal(signal.SIGINT, signal_handler)
-		print('Press Ctrl+C')
-		time.sleep(5);
-		for i, scan in enumerate(self.lidar.iter_scans()):
-	 		print('%d: Got %d measurments' % (i, len(scan)))
+  def run(self):
+    info = self.lidar.get_info()
+    print(info)
+    health = self.lidar.get_health()
+    print(health)
+    signal.signal(signal.SIGINT, self.signal_handler)
+    print('Press Ctrl+C')
+    time.sleep(5);
+    for i, scan in enumerate(self.lidar.iter_scans()):
+      print('%d: Got %d measurments' % (i, len(scan)))
 	 		
 if __name__ == "__main__":
-	lidar = RPLidar('/dev/ttyUSB0')
-	threadLidar=Lidar(lidar)
-	threadLidar.start()
-	threadLidar.join()
+  lidar = RPLidar('/dev/ttyUSB0')
+  threadLidar=Lidar(lidar)
+  threadLidar.start()
+  threadLidar.join()
 
 
 
