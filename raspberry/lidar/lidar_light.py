e7d3211c8ab95a3ccef6b18c20d098c3ae52858b
M. Nussli Gabriel <nussli@insa-07569>
2018-11-28 10:26:07 +0100

    lidar in a thread

--- raspberry/lidar/lidar_light.py
+++ raspberry/lidar/lidar_light.py
@@ -2,32 +2,39 @@ from rplidar import RPLidar
 import time
 import sys
 import signal
+from threading import Thread
 
-isRunning = 1
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    isRunning = 0
-    time.sleep(1)
-    lidar.stop()
-    lidar.stop_motor()
-    lidar.disconnect()
-    sys.exit(0)
+class Lidar(Thread)
+	def __init__(self, lidar):
+        Thread.__init__(self)
+        self.lidar=lidar
+        
+    def signal_handler(sig, frame):
+		print('You pressed Ctrl+C!')
+		self.lidar.stop()
+		self.lidar.stop_motor()
+		self.lidar.disconnect()
+		sys.exit(0)
+		
+    def run(self):
 
-lidar = RPLidar('/dev/ttyUSB0')
 
-info = lidar.get_info()
-print(info)
+		info = self.lidar.get_info()
+		print(info)
 
-health = lidar.get_health()
-print(health)
-signal.signal(signal.SIGINT, signal_handler)
-print('Press Ctrl+C')
-#signal.pause()
-time.sleep(5);
-for i, scan in enumerate(lidar.iter_scans()):
- if isRunning == 0:
-  print('breaking')
-  break
- else:
-  print('%d: Got %d measurments' % (i, len(scan)))
+		health = self.lidar.get_health()
+		print(health)
+		signal.signal(signal.SIGINT, signal_handler)
+		print('Press Ctrl+C')
+		time.sleep(5);
+		for i, scan in enumerate(self.lidar.iter_scans()):
+	 		print('%d: Got %d measurments' % (i, len(scan)))
+	 		
+if __name__ == "__main__":
+	lidar = RPLidar('/dev/ttyUSB0')
+	threadLidar=Lidar(lidar)
+	threadLidar.start()
+	threadLidar.join()
+
+
 
