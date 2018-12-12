75ec7c1013f0ddc37c5d7717373e01855375d301
Gabriel Nussli <nussli@insat.insa-toulouse.fr>
2018-12-12 13:54:29 +0100

    yo

--- raspberry/lidar_fused_test.py
+++ raspberry/lidar_fused_test.py
@@ -12,6 +12,8 @@ import lidar
 import can
 import os
 import struct
+from lidar_light import Lidar
+from autonome3 import MySend
 
 HOST = ''                # Symbolic name meaning all available interfaces
 PORT = 6666              # Arbitrary non-privileged port
@@ -23,6 +25,9 @@ US2 = 0x001
 OM1 = 0x101
 OM2 = 0x102
 
+VOL_GAUCHE=0
+VOL_DROIT=0
+VOL_CENTRE=0
 
 '''
     Messages envoyés :
@@ -61,180 +66,11 @@ OM2 = 0x102
     - Contra l de l'avancée
     header : MOV payload : forward | backward | stop
     '''
-
-class MySend(Thread):
-    
-    detectObstacle = False
-    detectObstacleOld = False
-    detectObstacleAD = False
-    detectObstacleAG = False
-    detectObstacleAC = False
-    detectObstacleACproche = False
-    differentiel = False
-    distanceDetectObstacleAD = 30
-    distanceDetectObstacleAG = 30
-    distanceDetectObstacleACproche = 30
-    distanceDetectObstacleAC = 150
-    
-    def __init__(self, bus):
-        Thread.__init__(self)
-        self.bus = bus
-    
-    def run(self):
-        
-        self.speed_cmd = 30
-        self.move = 0
-        self.turn = 0
-        self.enable = 0
-        
-        while True :
-            
-            msg = self.bus.recv()
-            
-            # print(msg.arbitration_id, msg.data)
-            # print("Reading")
-            
-            
-            
-            st = ""
-            
-            if msg.arbitration_id == US1:
-                
-                # ultrason avant gauche
-                distance = int.from_bytes(msg.data[0:2], byteorder='big')
-                message = "UFL:" + str(distance) + ";"
-                if distance < MySend.distanceDetectObstacleAG:
-                    MySend.detectObstacleAG=True
-                else: MySend.detectObstacleAG=False
-                print(message)
-                
-                # ultrason avant droit
-                distance = int.from_bytes(msg.data[2:4], byteorder='big')
-                message = "UFR:" + str(distance)+ ";"
-                if distance < MySend.distanceDetectObstacleAD:
-                    MySend.detectObstacleAD = True
-                else: MySend.detectObstacleAD = False
-                print(message)
-            
-                # ultrason avant centre
-                distance = int.from_bytes(msg.data[4:6], byteorder='big')
-                message = "URC:" + str(distance)+ ";"
-                if distance < MySend.distanceDetectObstacleACproche:
-                    MySend.detectObstacleACproche = True
-                elif distance<MySend.distanceDetectObstacleAC:
-                    MySend.detectObstacleAC = True
-                else:
-                    MySend.detectObstacleAC = False
-                    MySend.detectObstacleACproche = False
-
-                print(message)
-                MySend.detectObstacleOld = MySend.detectObstacle
-                MySend.detectObstacle = MySend.detectObstacleAC #pour l'instant on regarde que les obstacles en face
-            
-            elif msg.arbitration_id == MS:
-                # position volant
-                position_volant = int.from_bytes(msg.data[0:2], byteorder='big')
-                message = "POS:" + str(position_volant)+ ";"
-                #print(message)
-            
-                # detection obstacle lointain avec ultrason avant centre; dans ce cas on tourne à droite
-                if MySend.detectObstacle and not(MySend.detectObstacleAG) and not(MySend.detectObstacleAD) and not(MySend.detectObstacleACproche):
-                    self.move = 1
-                    self.enable = 1
-                    differentiel = False
-                    if ( MySend.detectObstacle == MySend.detectObstacleOld ): #checke que c'est une valeur plausible et pas juste une erreur de passage
-                        self.move = 1
-                        self.enable = 1
-                        differentiel = True
-                        if (position_volant > 1350):
-                            self.turn = -1
-                        else:
-                            self.turn = 0
-                # detection obstacle proche dans ce cas on s'arrête
-                elif MySend.detectObstacleAG or MySend.detectObstacleAD or MySend.detectObstacleACproche:
-                    self.move = 0
-                    self.enable = 0
-                # si pas d'obstacle on va tout droit
-                else:
-                    self.move = 1
-                    self.enable = 1
-                    differentiel = False
-                    # permet de "rester droit"
-                    if (position_volant < 1600):
-                        self.turn = 1
-                    elif (position_volant > 1700):
-                        self.turn = -1
-                    else:
-                        self.turn = 0
-    
-                #calcul des commandes de mouvement
-                if self.enable:
-                    if differentiel :
-                        cmd_mv_droit = (50 - self.move*self.speed_cmd - 10) | 0x80   #marche arrière
-                        cmd_mv_gauche = (50 + self.move*self.speed_cmd) | 0x80
-                    else:
-                        cmd_mv_droit = (50 + self.move*self.speed_cmd) | 0x80
-                        cmd_mv_gauche = (50 + self.move*self.speed_cmd) | 0x80
-                    cmd_turn = 50 + self.turn*20 | 0x80
-                else:
-                    cmd_mv_droit = (50 + self.move*self.speed_cmd) & ~0x80
-                    cmd_mv_gauche = (50 + self.move*self.speed_cmd) & ~0x80
-                    cmd_turn = 50 + self.turn*20 & 0x80
-            
-            #if (st!=""):print(st)
-            
-                msg = can.Message(arbitration_id=MCM,data=[cmd_mv_gauche, cmd_mv_droit, cmd_turn,0,0,0,0,0],extended_id=False)
-                self.bus.send(msg)
-
-
-
-# Lidar Thread
-
-
-class Lidar(Thread):
-  def __init__(self, lidar):
-    Thread.__init__(self)
-    self.shutdown_flag = threading.Event()
-    self.lidar=lidar
-  def run(self):
-    info = self.lidar.get_info()
-    print(info)
-    health = self.lidar.get_health()
-    print(health)
-    
-    time.sleep(5);
-    for i, scan in enumerate(self.lidar.iter_scans()):
-      if self.shutdown_flag.is_set():
-        print('please wait, lidar is shuting down')
-        break
-      else:
-        print('%d: Got %d measurments' % (i, len(scan)))
-
-lidar = RPLidar('/dev/ttyUSB0')
-
-threadLidar=Lidar(lidar)
-
-def signal_handler(sig, frame):
-  print('You pressed Ctrl+C!')
-  threadLidar.shutdown_flag.set()
-  time.sleep(5)
-  lidar.stop()
-  lidar.stop_motor()
-  lidar.disconnect()
-
-
-
-
-
 if __name__ == "__main__":
     print('Bring up CAN0....')
     os.system("sudo /sbin/ip link set can0 down")
     os.system("sudo /sbin/ip link set can0 up type can bitrate 400000")
     time.sleep(0.1)
-    
-    threadLidar.start()
-    signal.signal(signal.SIGINT, signal_handler)
-    print('Press Ctrl+C')
     
     try:
         bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
@@ -243,9 +79,42 @@ if __name__ == "__main__":
         exit()
 
 
+    #gauche
+    msg = can.Message(arbitration_id=MCM,data=[0, 0, 0xE4,0,0,0,0,0],extended_id=False)
+    bus.send(msg)
+    time.sleep(0.75)
+    msg = can.Message(arbitration_id=MCM,data=[0, 0, 0,0,0,0,0,0],extended_id=False)
+    bus.send(msg)
+    time.sleep(0.5)
+    msg1=bus.recv()
+    while not (msg1.arbitration_id == MS):
+        msg1=bus.recv()
+    VOL_GAUCHE = int.from_bytes(msg1.data[0:2], byteorder='big')
+
+    bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+
+    #droit
+    msg = can.Message(arbitration_id=MCM,data=[0, 0, 0x80,0,0,0,0,0],extended_id=False)
+    bus.send(msg)
+    time.sleep(0.75)
+    msg = can.Message(arbitration_id=MCM,data=[0, 0, 0,0,0,0,0,0],extended_id=False)
+    bus.send(msg)
+    msg2=bus.recv()
+    time.sleep(0.5)
+    while not(msg2.arbitration_id == MS):
+        msg2=bus.recv()
+    VOL_DROIT = int.from_bytes(msg2.data[0:2], byteorder='big')
+
+    VOL_CENTRE = int((VOL_GAUCHE+VOL_DROIT)/2)
+
+    print(VOL_DROIT)
+    print(VOL_GAUCHE)
+    print(VOL_CENTRE)
+
 
     newsend = MySend(bus)
     newsend.start()
-
-    newsend.join()
+    threadLidar.start()
+    signal.signal(signal.SIGINT, signal_handler)
     threadLidar.join()
+    newsend.join()
