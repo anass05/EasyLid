c75474c6f2f19c02bacb42adb7cfada3388f6bc3
M. Sadik Anass <sadik@insa-07422>
2019-01-08 16:44:22 +0100

    stop lidar when predict

--- raspberry/autonome3.py
+++ raspberry/autonome3.py
@@ -318,7 +318,7 @@ class MySend(Thread):
             #------------------------------------------------- ENVOI MESSAGE CAN ----------------------------------------------------
             if Lidar.leafStop==1:
                 msg = can.Message(arbitration_id=MCM,data=[0, 0, 0,0,0,0,0,0],extended_id=False)
-                self._is_running = False
+                sys.exit()
             else:
                 msg = can.Message(arbitration_id=MCM,data=[cmd_mv_gauche, cmd_mv_droit, cmd_turn,0,0,0,0,0],extended_id=False)
             self.bus.send(msg)
